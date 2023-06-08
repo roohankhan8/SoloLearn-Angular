6bf12976229967d54a0d1a39364a7733fbc21cbb
Roohan Khan <roohank8@gmail.com>
2023-06-08 10:51:11 -0700

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TransportationService } from './transportation.service';
+import { Car } from './cars'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  prefix = 'I\'m '
+  name = 'Roohan'
+  sentence() {
+    return this.prefix + this.name
+  }
+
+  colors: string[] = ['red', 'blue', 'green', 'purple'];
+  fruits: string[] = ['mango', 'apple', 'pear', 'banana'];
+
+  // subaru: Car = { make: 'Subaru', model: 'Outback', miles: 58232 };
+  // honda: Car = { make: 'Honda', model: 'Accord', miles: 39393 };
+  // bmw: Car = { make: 'BMW', model: 'X3', miles: 4400 };
+
+  cars: Car[];
+  make: string = '';
+  model: string = '';
+  miles: number = 0;
+  constructor(private transportationService: TransportationService) {
+    this.cars = this.transportationService.getCars()
+  }
+  addCar() {
+    const newCar: Car = { make: this.make.charAt(0).toUpperCase() + this.make.substring(1), model: this.model, miles: this.miles };
+    // const newCar: Car = { make: "Tesla", model: "X", miles: 100 };
+    this.transportationService.addCar(newCar);
+  }
+
+  saySomething() {
+    alert('good day.');
+  }
+
+  counter = 0;
+  increment() {
+    this.counter++;
+  }
+
+  phrase = "It's going";
+  update() {
+    this.phrase += " ..and going";
+  }
+
+  username: string = '';
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,34 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import "reflect-metadata";
+import { createConnection, Connection, getRepository } from "typeorm";
+import { User } from './app/entities/user';
 
+const connection = await createConnection({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "your-db-username",
+  password: "your-password",
+  database: "your-db",
+  entities: ["entities/*.js"]
+});
+const repository = getRepository(User);
+const user = new User();
+user.name = "Troy";
+user.email = "troy@sololearn.com";
+user.age = 25;
+await repository.save(user);
+const results = await repository.find({where: {name: "Troy"}});
+const result = await repository.findOne({where: {name: "Troy"}});
+const myUser = await repository.findOne({where: {name: "Troy"}});
+console.log(result, results, myUser)
+// myUser.email = "t@sololearn.com";
+// await repository.save(myUser);
+// const myUser = await repository.findOne({where: {name: "Troy"}});
+// await repository.delete(myUser.id);
+// await repository.delete({name: "Troy"});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
